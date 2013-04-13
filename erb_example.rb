b5801e00f2f5d4929882998048209a960f1c52b6
Kyle Gracey <kgracey1@gmail.com>
2013-04-13 14:09:01 -0500

    Steps 1 & 2 of Schneems' UT-Rails course

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,10 @@
 require 'erb'
 
+firstname = "Kyle"
+lastname = "Gracey"
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+myname = ERB.new "My name is <%= firstname %> <%= lastname %>"
+puts myname.result(binding)
